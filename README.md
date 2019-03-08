d8ae48b914ba04806122b07f446400fe714dc273
Zachary Yocum <zachary.yocum@me.com>
2019-03-07 21:15:27 -0500

    add program, README, and resources

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # paths
 Find solution to a graph traversal problem.
+
+## The Problem
+
+The goal is to find the path from a start node to an end node where edges between nodes have a cost associated with them.  The best path is that which minimizes the maximum cost of the highest cost edge along the path.
+
+The input format for specifying the graph nodes, edges, and the costs of edges is described here.
+
+Each line in the input represents a record with three integer values:
+
+| field | description |
+|-------|-------------|
+| 1 | node index |
+| 2 | node index |
+| 3 | edge cost |
+
+In this way, the edges in the graph are defined by pairs of node indices that are connected.
+
+An example is as follows:
+
+```
+0 1 3
+0 2 2
+1 2 1
+1 3 4
+2 3 5
+```
+
+In the above example, the cost of traversing from node `0` to node `1` is `3`, traversing from node `0` to node `2` is `2`, `1` to `2` is `1`, and so on.  While the input here is sorted by the record values, this is not necessary.  As long as a valid graph is serialized (i.e., there is a path from the start node to the end node), then the program will still find the optimal solution.
+
+The bidirectional graph, when visualized with edges labeld with their costs, looks like this:
+
+![graph.png](graph.png)
+
+## Usage
+To run the program, provide it with a text file with a graph description as previously described, and optionally target start and end nodes.  If a start node is not specified, the start node is assumed to be the node in the graph with the minimum index.  Similarly, if an end node is not specified, the end node is assumed to be the node in the graph with the maximum index.
+
+```
+$ ./paths.py -h
+usage: paths.py [-h] [-s START] [-e END] [-v] input
+
+Find solutions to a graph traversal problem. The goal is to find the path from
+a start node to an end node where edges between nodes have a cost associated
+with them. The best path is that which minimizes the maximum cost of the
+highest cost edge along the path.
+
+positional arguments:
+  input                 input data file (use "-" to read from stdin)
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s START, --start START
+                        start node in the graph (default: None)
+  -e END, --end END     end node in the graph (default: None)
+  -v, --verbose         show verbose output (default: False)
+$ cat data.txt 
+0 1 3
+0 2 2
+1 2 1
+1 3 4
+2 3 5
+$ ./paths.py data.txt
+4
+$ ./paths.py data.txt -v
+cost	path
+4	(0, 1, 3)
+4	(0, 2, 1, 3)
+5	(0, 2, 3)
+5	(0, 1, 2, 3)
+$ ./paths.py data.txt -v -s 1
+cost	path
+4	(1, 3)
+5	(1, 2, 3)
+5	(1, 0, 2, 3)
+$ ./paths.py data.txt -v -s 1 -e 0
+cost	path
+2	(1, 2, 0)
+3	(1, 0)
+5	(1, 3, 2, 0)
+```
---
+++ paths.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+"""Find solutions to a graph traversal problem.  The goal is to find the path from 
+a start node to an end node where edges between nodes have a cost associated 
+with them.  The best path is that which minimizes the maximum cost of the 
+highest cost edge along the path.
+"""
+
+def ngrams(iterable, n=1):
+    """Generate ngrams from an iterable
+    
+    l = range(5)
+    list(l) -> [0, 1, 2, 3, 4, 5]
+    list(ngrams(l, n=1)) -> [(0,), (1,), (2,), (3,), (4,)]
+    list(ngrams(l, n=2)) -> [(0, 1), (1, 2), (2, 3), (3, 4)]
+    list(ngrams(l, n=3)) -> [(0, 1, 2), (1, 2, 3), (2, 3, 4)]
+    
+    """
+    return zip(*(iterable[i:] for i in range(n)))
+
+def load(data):
+    """Load graph and costs of edges from serialized data string"""
+    lines = data.splitlines()
+    graph = {}
+    costs = {}
+    for line in lines:
+        source, dest, cost = map(int, line.split())
+        if source not in graph:
+            graph[source] = set()
+        if dest not in graph:
+            graph[dest] = set()
+        graph[source].add(dest)
+        graph[dest].add(source)
+        costs[(source, dest)] = cost
+        costs[(dest, source)] = cost
+    return graph, costs
+
+def paths(graph, start=None, end=None):
+    """From a given graph, generate all possible paths from the start node
+    to the end node."""
+    if start is None:
+        start = min(graph)
+    if end is None:
+        end = max(graph)
+    stack = [(start, [])]
+    while stack:
+        node, path = stack.pop()
+        # normally one would use path.append(node), but we do it this way
+        # because it creates a new list, instead of reusing the existing list
+        path = path + [node]
+        for neighbor in graph[node].difference(set(path)):
+            stack.append((neighbor, path))
+        if node == end and len(path) > 0:
+            yield tuple(path)
+
+def cost(path, costs):
+    """Compute the cost of the most costly edge along a given path"""
+    most_costly = max(ngrams(path, n=2), key=costs.get)
+    return costs[most_costly], len(path)
+
+def main(data, start=None, end=None, verbose=False):
+    """Sort each path by cost in ascending order"""
+    graph, costs = load(data)
+    results = sorted(
+        (cost(path, costs), path)
+        for path in paths(graph, start=start, end=end)
+    )
+    if verbose:
+        print('cost\tpath')
+        for result in results:
+            (cost_, length), path = result
+            print(cost_, path, sep='\t')
+    else:
+        ((min_cost, length), path), *_ = results
+        print(min_cost)
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description=__doc__
+    )
+    parser.add_argument(
+        'input',
+        type=argparse.FileType('r'),
+        help='input data file (use "-" to read from stdin)'
+    )
+    parser.add_argument(
+        '-s', '--start',
+        default=None,
+        type=int,
+        help='start node in the graph'
+    )
+    parser.add_argument(
+        '-e', '--end',
+        default=None,
+        type=int,
+        help='end node in the graph',
+    )
+    parser.add_argument(
+        '-v', '--verbose',
+        default=False,
+        action='store_true',
+        help='show verbose output'
+    )
+    args = parser.parse_args()
+    main(
+        args.input.read(),
+        start=args.start,
+        end=args.end,
+        verbose=args.verbose
+    )
+    if args.input.name != '<stdin>':
+        args.input.close()
